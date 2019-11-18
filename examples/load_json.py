35405c52b13b2d55565ebfd6036e980a1cd28196
LarsLongerich <lars.j.longerich@gmail.com>
2019-11-18 10:49:20 +0100

    Functional Calculate which processes the Condor values and returns them in the Condor dictionary

--- examples/load_json.py
+++ examples/load_json.py
@@ -1,20 +1,34 @@
 import json
 
 
-def correlate(p1, condor, panda):
-    print(p1)
-    print(condor)
-    print(panda)
-
 
 def main():
-    with open('p1_data.json', 'r') as f:
-        p1_data = json.load(f)
     with open('condor_data.json', 'r') as f:
         condor_data = json.load(f)
-    with open('panda_data.json', 'r') as f:
-        panda_data = json.load(f)
-    correlate(p1_data, condor_data, panda_data)
+    calc(condor_data)
+    print(json.dumps(condor_data, indent = 2))
+
+
+def calc(condor_data):
+    for i in condor_data:
+        detCPU = condor_data[i]["DetectedCpus"]
+        DetectedMemory = condor_data[i]["DetectedMemory"]
+        CCPU = sum(condor_data[i]["ChildCpus"])
+        CMemory = sum(condor_data[i]["ChildMemory"])
+
+        condor_data[i]["UsageCpus"] = float(float(CCPU) / float(detCPU))
+        condor_data[i]["UsageMemory"]= float(float(CMemory) / float(DetectedMemory))
+        condor_data[i]["ChildCpus"] = CCPU
+        condor_data[i]["ChildMemory"] = CMemory
+        condor_data[i]["Utilization"] = max(condor_data[i]["UsageCpus"] , condor_data[i]["UsageMemory"])
+        condor_data[i]["PerCPULoad"] = float(float(condor_data[i]["TotalCondorLoadAvg"]) / float(detCPU))
+
+
+    return(condor_data)
+
+
+
+
 
 if __name__ == "__main__":
     main()
